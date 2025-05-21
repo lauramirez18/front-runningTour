146e99e6d91679f00b6d2de0e22c98eae431496f
Andres sierra garcia <andresfelipe3666@gmail.com>
2025-05-21 15:26:38 -0500

    update4334

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,50 +1,47 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import  MainLayout from '../layouts/MainLayout.vue'
-import Dashboard from '../views/Dashboard.vue'
-import Login from '../views/Login.vue'
-import Form from '../views/Form.vue'
-import ClosedForm from '../views/ClosedForm.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import MainLayout from "../layouts/MainLayout.vue";
+import Dashboard from "../views/Dashboard.vue";
+import Login from "../views/Login.vue";
+import Form from "../views/Form.vue";
+import ClosedForm from "../views/ClosedForm.vue";
 
 const routes = [
     {
-        path: '/login',
-        name: 'Login',
-        component:Login,
+        path: "/login",
+        name: "Login",
+        component: Login,
     },
     {
-        path: '',
+        path: "",
         component: MainLayout,
-        children:[
-              {
-                path: '',
-                name: 'form',
-                component: Form
-              },
+        children: [
             {
-                path:'/dashboard',
-                name: 'Dashboard',
-                component: Dashboard,
-            },
-            
-            {
-                path:'/form',
-                name: 'Form',
+                path: "",
+                name: "form",
                 component: Form,
             },
-            
-           /*  {
-                path:'/form',
-                name: 'closedForm',
-                component:  ClosedForm,
-            }, */
-        ]               
+            {
+                path: "/dashboard",
+                name: "Dashboard",
+                component: Dashboard,
+            },
 
+            {
+                path: "/form",
+                name: "Form",
+                component: Form,
+            },
+
+            /*  {
+                      path:'/form',
+                      name: 'closedForm',
+                      component:  ClosedForm,
+                  }, */
+        ],
     },
-    
-]
+];
 
 export const router = createRouter({
     history: createWebHistory(),
-    routes
-})
-   
+    routes,
+});
